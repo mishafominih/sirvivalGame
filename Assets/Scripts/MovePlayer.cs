2e2b7b800d64074cd58b5d041e5f5965bf2b6bbc
mishafominih <mishagjxn@mail.ru>
2020-09-12 12:05:35 +0500

    Initial commit

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 1;
+    Rigidbody rb;
+    Camera cam;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    private Vector3 GetDirection(float angle)
+    {
+        var z = -Mathf.Sin(angle / 180 * Mathf.PI);
+        var x = Mathf.Cos(angle / 180 * Mathf.PI);
+        return new Vector3(x, 0, z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            var position = GetDirection(transform.rotation.eulerAngles.y - 90) / 10 * speed;
+            rb.MovePosition(position + transform.position);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            var position = GetDirection(transform.rotation.eulerAngles.y) / 10 * speed;
+            rb.MovePosition(position + transform.position);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            var position = GetDirection(transform.rotation.eulerAngles.y + 90) / 10 * speed;
+            rb.MovePosition(position + transform.position);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            var position = GetDirection(transform.rotation.eulerAngles.y + 180) / 10 * speed;
+            rb.MovePosition(position + transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/RotatePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    Camera cam;
+    public float sensitivityX = 1;
+    public float sensitivityY = 1;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = false;
+        var x = Input.GetAxis("Mouse X");
+        var y = Input.GetAxis("Mouse Y");
+        transform.Rotate(0, x * sensitivityX, 0);
+        cam.transform.Rotate(-y * sensitivityY, 0, 0);
+    }
+}
