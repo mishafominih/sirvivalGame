c2d0485aec328eb52d7359c969a34d8780b26ecd
mishafominih <mishagjxn@mail.ru>
2020-09-15 19:11:13 +0500

    добавил смену оружия
    следующая задача: реализовать возможность подбирать оружие, ложить в инвентарь и т д

---
+++ Assets/Scripts/ManagerWearons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerWearons : MonoBehaviour
+{
+    public List<GameObject> Wearons;
+    private GameObject wearonNow;
+    private int index = 0;
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+        if(Wearons.Count > 0)
+        {
+            wearonNow = Instantiate(Wearons[0], cam.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.mouseScrollDelta.magnitude > 0 && Wearons.Count > 1)
+        {
+            Destroy(wearonNow);
+            if (index == Wearons.Count - 1)
+            {
+                wearonNow = Instantiate(Wearons[0], cam.transform);
+                index = 0;
+            }
+            else
+            {
+                wearonNow = Instantiate(Wearons[++index], cam.transform);
+            }
+        }
+    }
+}
