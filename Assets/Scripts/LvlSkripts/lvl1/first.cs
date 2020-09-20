cca6a0c2d847f4bf29bed02a09ec9e8b433ad35e
mishafominih <mishagjxn@mail.ru>
2020-09-20 23:31:02 +0500

    продолжил проработку уровня
    в плане логики все норм, но вот с точки зрения дизайна все плохо. Модели все разностилевые, иногда нужных моделей вообще нету. Поэтому без 3д моделлеров написать свою игру вообще не варик. незнаю, что будет дальше, но пока все как то не очень. Но для знакомства и пополнения резюме, сойдет!

---
+++ Assets/Scripts/LvlSkripts/lvl1/first.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first : MonoBehaviour
+{
+    public List<GameObject> zombi;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            zombi.ForEach(z =>
+            {
+                z.GetComponent<VolfMove>().target = other.gameObject;
+            });
+        }
+    }
+}
