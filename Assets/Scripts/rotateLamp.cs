695550a5fd0c5afb0598bfd609326862406f3614
mishafominih <mishagjxn@mail.ru>
2020-09-23 09:27:57 +0500

    добавил реалистичное(насколько это возможно) освещение
    хотел найти хорушую модельку но так и не нашел, пришлось самому делать(

---
+++ Assets/Scripts/rotateLamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateLamp : MonoBehaviour
+{
+    public float Speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed += Random.Range(-1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Speed, 0);
+    }
+}
