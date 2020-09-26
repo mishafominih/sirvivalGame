e1a4cc74aa91eca1a1de126993ee9bcc72436ca1
mishafominih <mishagjxn@mail.ru>
2020-09-26 17:40:08 +0500

    добавил атаку врагов, теперь игрок может умереть((
    исправил баги))

--- Assets/Scripts/Life.cs
+++ Assets/Scripts/Life.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Life : MonoBehaviour
 {
     public float hp = 100;
     private float life;
-    public bool Alive;
+    public bool Alive = true;
     public float TimeDestroy = 10;
+    public Scrollbar scr;
     Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.tag = "target";
+        if(gameObject.tag != "Player") gameObject.tag = "target";
         life = hp;
         anim = GetComponent<Animator>();
         Alive = true;
@@ -21,6 +23,11 @@ public class Life : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(scr != null)
+        {
+            scr.GetComponent<Scrollbar>().size = 1 - life / 100;
+            scr.GetComponent<Scrollbar>().value = 0;
+        }
         if(life <= 0)
         {
             Destroy(gameObject, TimeDestroy);
--- Assets/Scripts/VolfMove.cs
+++ Assets/Scripts/VolfMove.cs
@@ -10,10 +10,15 @@ public class VolfMove : MonoBehaviour
 	public float InstatiateTime = 3;
 	public float speed = 0.02f;
 	public float Distance = 45;
+    public float FirstAttackTime = 0.5f;
+    public float AttackTime = 3;
+    public float Damage = 10;
 	WolfMoveLogic logic;
 	Life life;
     new WolfAnimation animation;
 	Timer timer;
+	private bool attack = false;
+	private Timer attackTimer;
 	
 	private bool GetDistanceToTarget()
     {
@@ -36,11 +41,33 @@ public class VolfMove : MonoBehaviour
 			{
 				if (!GetDistanceToTarget()) logic.end = false;
 				if (life.Alive && GetDistanceToTarget())
-				{
-					logic.Move(target.transform.position);
-					animation.GetAnimation(logic.end, logic.Attack);
-				}
-			}
+                {
+                    logic.Move(target.transform.position);
+                    animation.GetAnimation(logic.end, logic.Attack);
+                    Attack();
+                }
+            }
 		}
+    }
+
+    private void Attack()
+    {
+        if (logic.Attack)
+        {
+            if (!attack)
+            {
+                attackTimer = new Timer(FirstAttackTime);
+                attack = true;
+            }
+            if (attackTimer.Check())
+            {
+                target.GetComponent<Life>().ChangeHp(Damage);
+                attackTimer = new Timer(AttackTime);
+            }
+        }
+        else
+        {
+            attack = false;
+        }
     }
 }
